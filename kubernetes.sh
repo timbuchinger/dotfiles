536ad188cc1614671dd1d375dfd0ec74f2d6126e
Tim Buchinger <timbuchinger@gmail.com>
2024-12-05 06:30:57 -0500

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 brew install siderolabs/tap/talosctl
 brew install kubecolor
 brew install kubectx
